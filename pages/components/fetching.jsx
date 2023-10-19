45643ad22e09f04d7afe92b41466233d56476a6d
JuanBG20 <juanbgalvan.19@gmail.com>
2023-10-19 10:37:33 -0300

    BaseURL

--- pages/components/fetching.jsx
+++ pages/components/fetching.jsx
@@ -1,5 +1,5 @@
 export default function fetchProducts(baseURL, products, setProducts) {
-  fetch(`${baseURL}/products`)
+  fetch("https://stock-app-back-dev-cchq.2.us-1.fl0.io/api/v1/products")
     .then((res) => res.json())
     .then(({ products }) => setProducts(products));
 }
